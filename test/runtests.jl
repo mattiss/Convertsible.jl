51418401420c6a6511f408b5888ba4ff5eaadd30
Matth Tiss <Matthieu.Tisserand@polytechnique.org>
2022-12-10 17:23:17 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using Convertsible
 using Test
 
 @testset "Convertsible.jl" begin
-    # Write your tests here.
+
+    @testset "Testing Type Conversions" begin
+        @testset "Casting '$s' to Bool" for s in ["true", "True", "TRUE", "yeS", "YES", "yes"]
+            @test convert(Bool, s) == true
+        end
+        @testset "Casting '$s' to Bool" for s in ["false", "False", "FALSE", "No", "NO", "zobi"]
+            @test convert(Bool, s) == false
+        end
+    end    
+    
 end
