c758defb09158a59f337db732ae942a9dcd40288
Matthieu Tisserand <matthieu.tisserand@polytechnique.org>
2022-12-11 11:31:42 +0100

    Refactored DataFrame tests. Added load_csv test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,42 +4,69 @@ using DataFrames
 
 @testset "Convertsible.jl" begin
 
-    df_tests = DataFrame(Id = Int[], Name = String[], Age = Int[])
-    push!(df_tests, [1 "John Doe" 35])
-
     @testset "Testing Playbook actions" begin
+
+        @testset "Load csv" begin
+            df = DataFrame()
+            Convertsible.load_csv(df, filepath="data/csv/sample_data.csv")
+            df_expected = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35))
+            @test isequal(df_expected, df) 
+        end
+
         @testset "Select columns" begin
-            df = copy(df_tests)
-            Convertsible.select_columns(df, columns = ["Name", "Id"])
-            df_expected = DataFrame(Name = String[], Id = Int[])
-            push!(df_expected, ["John Doe" 1])
-            @test isequal(df, df_expected) 
+            df = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35))
+            Convertsible.select_columns(df, columns=["Name", "Id"])
+            df_expected = DataFrame(("Name"=>"John Doe"),("Id"=>1))
+            @test isequal(df_expected, df) 
         end
 
         @testset "Rename columns" begin
-            df = copy(df_tests)
-            Convertsible.rename_columns(df, rename = Dict("Name" => "NewName", "Age" => "NewAge"))
-            df_expected = DataFrame(Id = Int[], NewName = String[], NewAge = Int[])
-            push!(df_expected, [1 "John Doe" 35])
-            @test isequal(df, df_expected) 
+            df = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35))
+            Convertsible.rename_columns(df, rename=Dict("Name" => "NewName", "Age" => "NewAge"))
+            df_expected = DataFrame(("Id"=>1),("NewName"=>"John Doe"),("NewAge"=>35))
+            @test isequal(df_expected, df) 
         end
 
         @testset "Normalize columns" begin
-            df = copy(df_tests)
-            df[!, :"bad looking column name"] = [0]
-            Convertsible.normalize_columns(df, method = "pascal_case")
-            df_expected = DataFrame(Id = Int[], Name = String[], Age = Int[], BadLookingColumnName = Int[])
-            push!(df_expected, [1 "John Doe" 35 0])
-            @test isequal(df, df_expected) 
+            df = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35),("bad looking column name"=>missing))
+            Convertsible.normalize_columns(df, method="pascal_case")
+            df_expected = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35),("BadLookingColumnName"=>missing))
+            @test isequal(df_expected, df) 
         end
 
-        @testset "Convert columns types" begin
-            df = copy(df_tests)
+        @testset "Convert columns types" begin 
+            df = DataFrame(("Id"=>1),("Name"=>"John Doe"),("Age"=>35))
             Convertsible.convert_columns(df, types = Dict("Id" => "String", "Name" => "Bool", "Age" => "Double"))
-            df_expected = DataFrame(Id = String[], Name = Bool[], Age = Float64[])
-            push!(df_expected, ["1" false 35.0])
-            @test isequal(df, df_expected) 
+            df_expected = DataFrame(("Id"=>"1"),("Name"=>false),("Age"=>35.0))
+            @test isequal(df_expected, df) 
         end
+
+        @testset "Load csv with partially missing data" begin
+            df = DataFrame()
+            Convertsible.load_csv(df, filepath="data/csv/partially_missing_data.csv", types=Dict("Age"=>Int))
+            df_expected = DataFrame(Id = Int[], Name = String[], Age = Union{Missing, Int}[], Income = Int[])
+            push!(df_expected, [1 "John Doe" missing 100000])
+            push!(df_expected, [2 "Harry Cover" 24 230000])
+            @test isequal(df_expected, df) 
+
+            # Convertsible.convert_columns(df, types = Dict("Age" => "Double"))
+            # df_expected = DataFrame(Id = Int[], Name = String[], Age = Float64[], Income = Int[])
+            # push!(df_expected, [1 "John Doe" missing 100000])
+            # @test isequal(df_expected, df) 
+        end       
+
+        @testset "Load csv with totally missing data" begin
+            df = DataFrame()
+            Convertsible.load_csv(df, filepath="data/csv/totally_missing_data.csv", types=Dict("Age"=>Int))
+            df_expected = DataFrame(Id = Int[], Name = String[], Age = Union{Missing, Int}[], Income = Int[])
+            push!(df_expected, [1 "John Doe" missing 100000])
+            @test isequal(df_expected, df) 
+
+            # Convertsible.convert_columns(df, types = Dict("Age" => "Double"))
+            # df_expected = DataFrame(Id = Int[], Name = String[], Age = Float64[], Income = Int[])
+            # push!(df_expected, [1 "John Doe" missing 100000])
+            # @test isequal(df_expected, df) 
+        end       
     end
 
     @testset "Testing Type Conversions" begin
